07aaa93eb6738294a6c0dc9b040c928480ab09cb
Ilmir <ilmirsharifullin2000@gmail.com>
2024-04-04 16:55:09 +0300

    fix start message

--- src/telegram/handlers/start.py
+++ src/telegram/handlers/start.py
@@ -9,5 +9,7 @@ router = Router(name=__name__)
 
 @router.message(CommandStart())
 async def start_cmd(message: Message):
-    text = 'Текст для старта'
+    text = '''Здравствуйте, этот тест направлен на определение вашего темперамента, что в дальнейшем поможет нам лучше определить ваш стиль одежды.
+
+Для того, чтобы начать тест нажмите на кнопку "Начать тест"'''
     await message.answer(text, reply_markup=main_menu)
